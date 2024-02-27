d2902db9b365392a519eae51edfdf7626497001e
quinlan9 <129864794+quinlan9@users.noreply.github.com>
2024-02-27 16:31:59 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,9 +16,6 @@ function App() {
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
 
-
-  /*将从API获取到的电影数据保存到movies状态中
-  setmovies是一个更新函数,用于更新movies状态*/
   const  getMovies = async () =>{
     try{
 
@@ -53,24 +50,7 @@ function App() {
   useEffect(() => {
     getMovies();
   },[])
-  /* useEffect本身不是箭头函数,是一个React Hook，接受两个参数：
-  第一个参数是一个函数（可以是箭头函数或普通函数），用于执行副作用操作；
-  第二个参数是一个依赖项数组，用来控制这个副作用函数何时执行
-  eg:
-  const [user, setUser] = useState(null);
-const userId = props.userId; // 假设这是从父组件传入的props
 
-useEffect(() => {
-  const fetchUserData = async () => {
-    const response = await fetch(`https://api.example.com/users/${userId}`);
-    const userData = await response.json();
-    setUser(userData);
-  };
-
-  fetchUserData();
-}, [userId]); // userId作为依赖项
-每当userId变化时，useEffect内的函数就会执行，从而获取并更新新用户的数据。
-  */ 
 
   return (
     <div className="App">
